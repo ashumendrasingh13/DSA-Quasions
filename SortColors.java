5bfc5c04a213d48f331f909d405637e34a9b9502
Ashumendra Singh <113494766+ashumendrasingh13@users.noreply.github.com>
2022-12-07 21:45:56 +0530

    Update SortColors.java

--- SortColors.java
+++ SortColors.java
@@ -1,6 +1,27 @@
+//SOLUTION 1-->>
+
 class Solution {
     public void sortColors(int[] nums) {
         Arrays.sort(nums);
+        
+    }
+}
+
+
+//SOLUTION 2-->>
+
+class Solution {
+    public void sortColors(int[] array) {
+        for(int i = 1; i < array.length; i++){
+            int keyValue = array[i];
+            int j = i - 1;
+            while(j >= 0 && array[j] > keyValue){
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = keyValue;
+
+        }
         
     }
 }

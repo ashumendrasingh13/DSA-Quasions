eed19030af8c8039803bc165b0705f5898aa6986
Ashumendra Singh <113494766+ashumendrasingh13@users.noreply.github.com>
2023-05-30 20:57:55 +0530

    Create Code2.java

---
+++ LinkedList/Code2.java
@@ -0,0 +1,82 @@
+class Node {
+    int data;
+    Node next;
+};
+
+class LinkedList {
+  Node head;
+
+  LinkedList(){
+    head = null;
+  }
+  void push_back(int newElement) {
+    Node newNode = new Node();
+    newNode.data = newElement;
+    newNode.next = null; 
+    if(head == null) {
+      head = newNode;
+    } else {
+      Node temp = new Node();
+      temp = head;
+      while(temp.next != null)
+        temp = temp.next;
+      temp.next = newNode;
+    }    
+  }
+  void push_at(int newElement, int position) {     
+    Node newNode = new Node(); 
+    newNode.data = newElement;
+    newNode.next = null;
+
+    if(position < 1) {
+      System.out.print("\nposition should be >= 1.");
+    } else if (position == 1) {
+      newNode.next = head;
+      head = newNode;
+    } else {
+      
+      Node temp = new Node();
+      temp = head;
+      for(int i = 1; i < position-1; i++) {
+        if(temp != null) {
+          temp = temp.next;
+        }
+      }
+   
+      if(temp != null) {
+        newNode.next = temp.next;
+        temp.next = newNode;  
+      } else {
+        System.out.print("\nThe previous node is null.");
+      }       
+    }
+  } 
+  void PrintList() {
+    Node temp = new Node();
+    temp = this.head;
+    if(temp != null) {
+      System.out.print("The list contains: ");
+      while(temp != null) {
+        System.out.print(temp.data + " ");
+        temp = temp.next;
+      }
+      System.out.println();
+    } else {
+      System.out.println("The list is empty.");
+    }
+  }    
+
+  public static void main(String[] args) {
+    LinkedList MyList = new LinkedList();
+    MyList.push_back(3);
+    MyList.push_back(6);
+    MyList.push_back(5);
+    MyList.PrintList();
+
+    MyList.push_at(8, 2);
+    MyList.PrintList();
+
+    MyList.push_at(9, 3);
+    MyList.PrintList();      
+  }
+}
